767214ea03507bb1e2dbdcbc215db08f3bb37285
Arseniy Sokolov <sokolovaa@m2.ru>
2020-12-17 01:50:37 +0300

    feat(*): update bot

---
+++ src/bff/telegram-bot/constants.ts
@@ -0,0 +1,29 @@
+import * as TGB from 'node-telegram-bot-api';
+
+export const SALES_KEYBOARD_CONFIG: TGB.InlineKeyboardButton[][] = [
+    [
+        {
+            text: '🎁 Показать акцию "Товар дня (новогодняя)"',
+            callback_data: 'getNewYearSale'
+        }
+    ],
+    [
+        {
+            text: 'Обновить сейчас',
+            callback_data: 'forceUpdate'
+        }
+    ]
+];
+
+export const GREETING_MESSAGE = 'Привет! 🥭 Сегодня я могу рассказать про акции Вкусвилла.\nДанные обновляются дважды в сутки.';
+
+export const SALE_MESSAGE = ({ caption, url, salesPrice, price }: Partial<Good>, date: Date) => {
+    const formattedDate = `${date.toLocaleDateString('ru-RU')}, ${date.toLocaleTimeString()}`;
+    return `
+<strong>${caption}</strong> 
+<a href="${url}">Перейти на сайт ↗️</a>\n
+Последнее обновление: ${formattedDate}
+Цена по акции: ${salesPrice ? `<strong>${salesPrice} руб.</strong>` : 'не указана'}
+Обычная цена: ${price} руб.
+`;
+}
--- src/bff/telegram-bot/telegram-bot.service.ts
+++ src/bff/telegram-bot/telegram-bot.service.ts
@@ -1,19 +1,56 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import * as TelegramBot from 'node-telegram-bot-api';
+import * as TGB from 'node-telegram-bot-api';
+import { ShopParserService } from '../shop-parser/shop-parser.service';
+import { SALE_MESSAGE, GREETING_MESSAGE, SALES_KEYBOARD_CONFIG } from './constants';
 
 @Injectable()
 export class TelegramBotService implements OnModuleInit {
-    private bot: TelegramBot;
+    private bot: TGB;
+    private chatId: number;
 
-    constructor() {
-        this.bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+    constructor(private shopParserService: ShopParserService) {
+        this.bot = new TGB(process.env.TELEGRAM_BOT_TOKEN, { polling: true, filepath: false, });
     }
 
     onModuleInit() {
+        this.initialize();
+    }
+
+    private initialize() {
         this.bot.on('message', (msg) => {
-            console.log(msg);
-            const chatId = msg.chat.id;
-            this.bot.sendMessage(chatId, 'Привет, Друг!');
+            this.chatId = msg.chat.id;
+            this.sendGreeting();
+        });
+        this.handleKeyboardInput();
+    }
+
+    private handleKeyboardInput() {
+        this.bot.on('callback_query', (query) => {
+            switch (query.data) {
+                case 'getNewYearSale':
+                    this.sendSale();
+                    break;
+                case 'forceUpdate':
+                    this.sendSale();
+                    break;
+            }
+        });
+    }
+
+    private sendSale() {
+        const share = this.shopParserService.getShare();
+        const { imgSrc, ...good } = share.goods[0];
+        this.bot.sendPhoto(this.chatId, imgSrc, {
+            caption: SALE_MESSAGE(good, new Date(share.dates[0])),
+            parse_mode: 'HTML'
+        });
+    }
+
+    private sendGreeting() {
+        this.bot.sendMessage(this.chatId, GREETING_MESSAGE, {
+            reply_markup: {
+                inline_keyboard: SALES_KEYBOARD_CONFIG
+            },
         });
     }
 }
